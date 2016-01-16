832a59916101fa852db3e1a4d93d7ce5b7c3a74d
h_shirai <h_shirai@nullpot.com>
2016-01-16 14:58:36 +0900

    initial

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" class=" is-copy-enabled is-u2f-enabled">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#">
+	<meta charset='utf-8'>
+	<meta name="viewport" content="width=1020">
+	<title>why</title>
+
+	<link rel="stylesheet" href="/css/style.css">
+	<script src="/js/common.js" type="text/javascript"></script>
+</head>
