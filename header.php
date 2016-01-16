e798200683a29d9a13e1f2e27e3288d695a2e90e
h_shirai <h_shirai@nullpot.com>
2016-01-16 15:54:19 +0900

    topページとりあえず版

--- header.php
+++ header.php
@@ -3,8 +3,10 @@
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object# article: http://ogp.me/ns/article# profile: http://ogp.me/ns/profile#">
 	<meta charset='utf-8'>
 	<meta name="viewport" content="width=1020">
-	<title>why</title>
+	<title>なぜ地獄</title>
 
 	<link rel="stylesheet" href="/css/style.css">
-	<script src="/js/common.js" type="text/javascript"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/common.js" type="text/javascript"></script>
 </head>
+
--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php require_once('header.php'); ?>
 
 <body>
+
+<header>
+	<h1>なぜ地獄
+		<small>ほげほげ</small>
+	</h1>
+</header>
+<h2>あなたのお悩みなんてーの</h2>
+	<input id="inputbox" type="text">
+	<button id="submit_first" type="button">なぜ地獄の悩みから開放されたい</button>
+
+
 </body>
+
+<script type="text/javascript">
+	$('#submit_first').click(function(){
+		console.log($('#inputbox').val());
+
+	});
+</script>
+
 </html>
